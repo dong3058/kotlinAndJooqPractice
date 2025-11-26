21fd382228e28e9b06b316a390e20fb0458c0507
dong3058 <hwangdonggeun70@gmail.com>
2025-11-26 12:50:16 +0900

    test

--- src/test/kotlin/com/jooqtest/jooq/JooqApplicationTests.kt
+++ src/test/kotlin/com/jooqtest/jooq/JooqApplicationTests.kt
@@ -1,13 +1,24 @@
 package com.jooqtest.jooq
 
+import com.jooqtest.jooq.tables.records.UsersRecord
+import com.jooqtest.jooq.tables.references.USERS
+import org.assertj.core.api.Assertions
+import org.jooq.DSLContext
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class JooqApplicationTests {
 
+
+	@Autowired
+	lateinit var dslContext:DSLContext;
 	@Test
 	fun contextLoads() {
+
+	 var data=dslContext.selectFrom(USERS).fetch();
+		Assertions.assertThat(data.isNotEmpty).isTrue();
 	}
 
 }
